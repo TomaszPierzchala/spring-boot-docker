facda2c0a7fbfa7d9e3db9b019d771a6af0d00d5
Tomasz Pierzchała <tomasz.pierzchala@ailleron.com>
2020-05-29 19:52:44 +0200

    Added a "method" parameter to "@RequestMapping" annotations.

--- src/main/java/example/Application.java
+++ src/main/java/example/Application.java
@@ -7,6 +7,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
 import example.pdf.NormalDistribution;
@@ -19,20 +20,20 @@ public class Application {
 	@Autowired
 	private NormalDistribution normalPdf;
 	
-	@RequestMapping("/")
+	@RequestMapping(path = "/", method = RequestMethod.GET)
 	public String main() {
 		log.info("Called main path");
 		return "Choose {/gauss/,/standard} or {gauss/{mean}/{std_dev}, /normal/{mean}/{std_dev}}";
 	}
 
-	@RequestMapping({"/gauss", "/standard"})
+	@RequestMapping(path = {"/gauss", "/standard"}, method = RequestMethod.GET)
 	public String standardNormal() {
 		double normal = normalPdf.nextStandardNormal();
 		log.info("Generated random value x={} with Standard Normal pdf.", normal);
 		return "Generated random value " + normal + " with Standard Normal pdf.";
 	}
 	
-	@RequestMapping({"/gauss/{mean}/{std_dev}", "/normal/{mean}/{std_dev}"})
+	@RequestMapping(path = {"/gauss/{mean}/{std_dev}", "/normal/{mean}/{std_dev}"}, method = RequestMethod.GET)
 	public String normal(@PathVariable double mean, @PathVariable double std_dev) {
 		double normal = normalPdf.nextNormal(mean, std_dev);
 		
