0d0557c0f5e9aea1e71f68178bcb5e59be977e06
Tomasz Pierzchała <tomasz.pierzchala@ailleron.com>
2020-05-29 20:18:32 +0200

    Replaced "@RequestMapping(method = RequestMethod.GET)" with "@GetMapping"

--- src/main/java/example/Application.java
+++ src/main/java/example/Application.java
@@ -5,9 +5,8 @@ import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
 import example.pdf.NormalDistribution;
@@ -20,20 +19,20 @@ public class Application {
 	@Autowired
 	private NormalDistribution normalPdf;
 	
-	@RequestMapping(path = "/", method = RequestMethod.GET)
+	@GetMapping("/")
 	public String main() {
 		log.info("Called main path");
 		return "Choose {/gauss/,/standard} or {gauss/{mean}/{std_dev}, /normal/{mean}/{std_dev}}";
 	}
 
-	@RequestMapping(path = {"/gauss", "/standard"}, method = RequestMethod.GET)
+	@GetMapping({"/gauss", "/standard"})
 	public String standardNormal() {
 		double normal = normalPdf.nextStandardNormal();
 		log.info("Generated random value x={} with Standard Normal pdf.", normal);
 		return "Generated random value " + normal + " with Standard Normal pdf.";
 	}
 	
-	@RequestMapping(path = {"/gauss/{mean}/{std_dev}", "/normal/{mean}/{std_dev}"}, method = RequestMethod.GET)
+	@GetMapping({"/gauss/{mean}/{std_dev}", "/normal/{mean}/{std_dev}"})
 	public String normal(@PathVariable double mean, @PathVariable double std_dev) {
 		double normal = normalPdf.nextNormal(mean, std_dev);
 		
